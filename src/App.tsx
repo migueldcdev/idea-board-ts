0e4ff18607a9799e7a72d63067bdb26955bde539
miguel <miguel@tonio.dev>
2024-08-05 12:58:19 +0200

    implemented prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-
-
-
 function App() {
-  
-
   return (
-    <>       
+    <>
       <p className="text-cyan-500">
         Click on the Vite and React logos to learn more
       </p>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
