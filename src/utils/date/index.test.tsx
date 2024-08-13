ababd276e99a6d535701c6d646260b02b383ac24
miguel <miguel@tonio.dev>
2024-08-13 13:10:01 +0200

    implemented test each

--- src/utils/date/index.test.tsx
+++ src/utils/date/index.test.tsx
@@ -2,7 +2,11 @@ import { describe, expect, test } from "vitest";
 import { unixToDate } from ".";
 
 describe("Utils test", () => {
-  test("properly converts unix to redable date", () => {
-    expect(unixToDate(1723030052971)).toBe("7/8/2024");
+  test.each([
+    [1723030052971, "7/8/2024"],
+    [0, "1/1/1970"],
+    [-56, "1/1/1970"],
+  ])("properly converts unix to redable date", (unix, expected) => {
+    expect(unixToDate(unix)).toBe(expected);
   });
 });
