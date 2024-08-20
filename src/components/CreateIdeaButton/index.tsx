869924932073c668718113c521024d13020cd126
miguel <miguel@tonio.dev>
2024-08-20 14:30:00 +0200

    removed plus sign icon from button and added button tag

--- src/components/CreateIdeaButton/index.tsx
+++ src/components/CreateIdeaButton/index.tsx
@@ -4,20 +4,16 @@ import { ideasContext } from "../../context/ideasContext";
 
 import { Context } from "../../types";
 
-import { FaPlusCircle } from "react-icons/fa";
-
 export const CreateIdeaButton = () => {
   const { createIdea } = useContext(ideasContext) as Context;
 
   return (
-    <div className=" fixed bottom-0 w-full">
+    <div className=" fixed bottom-0 w-full bg-slate-100">
       <div className="bg-slate-900 blur-lg p-2 w-full"></div>
-      <div className="w-full flex justify-center p-4 bg-slate-100">
-        <FaPlusCircle
-          className="text-slate-900 text-4xl cursor-pointer hover:text-slate-800"
-          data-testid="create-button"
-          onClick={createIdea}
-        />
+      <div className="w-full flex justify-center">
+        <button className="p-4 rounded-full bg-indigo-900 hover:bg-indigo-800 mt-3 my-5 text-slate-100" onClick={createIdea}>
+          Create idea        
+        </button>
       </div>
     </div>
   );
