692d83a4e5aa5cc415356643be74b038474c2b7b
miguel <miguel@tonio.dev>
2024-08-23 09:41:38 +0200

    selfclose div in createIdeaButton

--- src/components/CreateIdeaButton/index.tsx
+++ src/components/CreateIdeaButton/index.tsx
@@ -8,8 +8,8 @@ export const CreateIdeaButton = () => {
   const { createIdea } = useContext(ideasContext) as Context;
 
   return (
-    <div className=" fixed bottom-0 w-full bg-slate-100">
-      <div className="bg-slate-900 blur-lg p-2 w-full"></div>
+    <div className=" fixed bottom-0 w-full bg-slate-100">      
+      <div className="bg-slate-900 blur-lg p-2 w-full" />
       <div className="w-full flex justify-center">
         <button
           className="p-4 rounded-full bg-indigo-800 hover:bg-indigo-700 mt-3 my-5 text-slate-100"
